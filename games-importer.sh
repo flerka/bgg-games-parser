dd758db94ea1ae71d8ff476a7acc3dac8373d39d
flerka <marya.kozhemyakina@gmail.com>
2020-02-13 18:42:43 +0200

    fix file naming, add new calculated column to csv

--- games-importer.sh
+++ games-importer.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 #git clone https://github.com/flerka/bgg-ranking-historicals
 
-get_new_file () {
+file_name= # used for function return values
+new_file_name='new_file.csv'
+
+fill_file_name () {
     current_day="$(date +%d)"
     while [[ "$current_day" != "0" ]]
     do
-        file_name="$(date +%Y)-$(date +%m)-${current_day}.csv"
+        day=$(printf %02d $current_day)
+        file_name="$(date +%Y)-$(date +%m)-${day}.csv"
         current_day=$((current_day-1))
 
         if [[ -f "$file_name" ]]; then
             echo "$file_name exist."
-            current_day=0
+            file_name=$file_name
+            return;
         else
             echo "$file_name not exist."
-        fi
-
-        
+        fi 
    done
+   
+   echo "there is no file with games"  
+   exit 1
 }
 
 main () {
@@ -26,7 +32,14 @@ main () {
     if [ $commits_count == 0 ]; then
         echo "Repository is up to date."
     else
-        echo "New files in repository."   
+        echo "New files in repository."  
+
         git pull
+        fill_file_name 
+        
+        # create new csv with additional column
+        awk -F"," 'FNR==1{a="bgg_id"} FNR>1{a=$8} {match(a, /\/([[:digit:]]+)\//)} {print $0","substr(a, RSTART+1, RLENGTH-2)}' $file_name > "$new_file_name"
     fi
 }
+
+main
